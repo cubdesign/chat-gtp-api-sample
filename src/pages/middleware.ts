97ca04b4b34d08fbeb5d18103e0cbff0a8aca8a1
cubdesign <takeo@cubdesign.com>
2023-03-19 11:32:13 +0900

    add basic auth

---
+++ src/pages/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/", "/index"],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  const isBasicAuth = process.env.BASIC_AUTH === "true" ? true : false;
+  const basicAuthUser = process.env.BASIC_AUTH_USER;
+  const basicAuthPassword = process.env.BASIC_AUTH_PASSWORD;
+
+  if (!isBasicAuth) {
+    return NextResponse.next();
+  }
+
+  if (basicAuthUser === undefined || basicAuthPassword === undefined) {
+    url.pathname = "/api/auth";
+    return NextResponse.rewrite(url);
+  }
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (user === basicAuthUser && pwd === basicAuthPassword) {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
+}
